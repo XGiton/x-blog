faed35b01532f686420dd7ce0950696b52b13dd4
xgiton <xiaozhidong@tungee.com>
2018-05-05 01:17:58 +0800

    添加profile

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,44 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-    <el-button type="primary" plain>主要按钮</el-button>
+    <div class="profile-content">
+      <Profile></Profile>
+    </div>
+    <div class="blog-content">
+      <img src="./assets/logo.png">
+      <router-view/>
+      <el-button type="primary" plain>主要按钮</el-button>
+    </div>
   </div>
 </template>
 
 <script>
+
+import Profile from './components/Profile'
+
 export default {
-  name: 'App'
+  name: 'App',
+
+  components: {
+    Profile
+  }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+}
+.profile-content {
+}
+.blog-content {
+  width: 80%;
 }
 </style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="profile">
+    <img class="avatar" src="../assets/avatar.jpg">
+    <div class="desc">
+      <h3 class="name">Joker</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+#profile {
+  height: 140px;
+  width: 100%;
+  background-color: #f0f0f0;
+}
+.desc {
+  float: left;
+  margin-left: 30px;
+  margin-top: 30px;
+  position: relative;
+}
+.desc .name {
+  font-size: 16px;
+}
+.avatar {
+  float: left;
+  width: 80px;
+  height: 80px;
+  margin-top: 30px;
+  margin-left: 30px;
+  position: relative;
+}
+</style>
